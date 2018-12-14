ba6e136bedb2568a79ffdbecb7ee27984682682b
Piotr Melkowski <piotr.melkowski@cognifide.com>
2018-12-14 14:09:07 +0100

    Add license headers and copied source information.

--- metrics-sender/src/main/java/io/knotx/metrics/graphite/GraphiteReporter.java
+++ metrics-sender/src/main/java/io/knotx/metrics/graphite/GraphiteReporter.java
@@ -1,5 +1,27 @@
+/*
+ * Copyright (C) 2018, Coda Hale, Yammer Inc.
+ * Copyright (C) 2018 Knot.x Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * This code was partially copied from metrics-graphite project by Coda Hale, Yammer Inc.
+ * The copied code:
+ *      GraphiteReporter.Builder class and its methods
+ * The new code:
+ *      GraphiteReporter's overridden and new methods
+ */
 package io.knotx.metrics.graphite;
-
 
 import java.util.Collections;
 import java.util.HashMap;
